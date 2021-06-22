756f0e70787beb856478d7ea373059f74158d1c8
eric <eric.wang@cargosmart.com>
2021-06-22 11:18:14 +0800

    fix feature envy 2: beauty the code

--- src/main/java/com/tw/academy/basic/$3_feature_envy/practiceTwo/LineItem.java
+++ src/main/java/com/tw/academy/basic/$3_feature_envy/practiceTwo/LineItem.java
@@ -1,14 +1,14 @@
 package com.tw.academy.basic.$3_feature_envy.practiceTwo;
 
 public class LineItem {
-	private String description;
-	private double price;
-	private int quantity;
+	private final String description;
+	private final double price;
+	private final int quantity;
 
-	public LineItem(String description, double p, int quantity) {
+	public LineItem(String description, double price, int quantity) {
 		super();
 		this.description = description;
-		this.price = p;
+		this.price = price;
 		this.quantity = quantity;
 	}
 
