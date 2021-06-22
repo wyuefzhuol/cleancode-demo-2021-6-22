cbe017a7624372307b2480b3f0f4837ba6ad25c1
eric <eric.wang@cargosmart.com>
2021-06-22 15:18:54 +0800

    refactor: introduce parameter object for sender in Parcel

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -11,12 +11,12 @@ public class Parcel {
     private final User receiver;
 
     public Parcel(String size, Double weight,
-                  User receiver, String senderName, String senderPhoneNumber, String senderAddress) {
+                  User receiver, User sender) {
         this.size = size;
         this.weight = weight;
-        this.senderName = senderName;
-        this.senderPhoneNumber = senderPhoneNumber;
-        this.senderAddress = senderAddress;
+        this.senderName = sender.getName();
+        this.senderPhoneNumber = sender.getPhoneNumber();
+        this.senderAddress = sender.getAddress();
 
         this.receiver = receiver;
     }
