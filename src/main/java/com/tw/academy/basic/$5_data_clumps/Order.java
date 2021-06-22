8e134a81154baa1362adcb35b69326b1b6879666
eric <eric.wang@cargosmart.com>
2021-06-22 14:49:30 +0800

    refactor: introduce update user name to buyer in Order

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -3,15 +3,15 @@ package com.tw.academy.basic.$5_data_clumps;
 public class Order {
 
     private final int orderNumber;
-    private final User user;
+    private final User buyer;
 
     public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
-        this.user = new User(buyerName, buyerPhoneNumber, buyerAddress);
+        this.buyer = new User(buyerName, buyerPhoneNumber, buyerAddress);
     }
 
     public String confirm(){
-        return user.getConfirmString();
+        return buyer.getConfirmString();
     }
 }
 
