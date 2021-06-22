9bed7aaeb2375b27bc1e0b1f811f0e7757dc21d6
eric <eric.wang@cargosmart.com>
2021-06-22 16:06:43 +0800

    refactor: remove not use field and method in address

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -7,19 +7,11 @@ public class Address {
         this.address = address;
     }
 
-    public String getFromAddress() {
-        return address;
-    }
-
     public String getCity() {
         return address.substring(address.indexOf("省") + 1, address.indexOf("市"));
     }
 
     public String getProvince() {
         return address.substring(0, address.indexOf("省"));
-    }
-
-    public String getAddress() {
-        return address;
     }
 }
