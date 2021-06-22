1af4123f5e0420c4105ebc9f72170cd2c64bdba5
eric <eric.wang@cargosmart.com>
2021-06-22 14:27:46 +0800

    refactor: introduce remove not use field in Order

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -2,21 +2,11 @@ package com.tw.academy.basic.$5_data_clumps;
 
 public class Order {
 
-    private int orderNumber;
-
-    private String buyerName;
-    private String buyerPhoneNumber;
-    private String buyerAddress;
-
-    private User user;
+    private final int orderNumber;
+    private final User user;
 
     public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
-
-        this.buyerName = buyerName;
-        this.buyerPhoneNumber = buyerPhoneNumber;
-        this.buyerAddress = buyerAddress;
-
         this.user = new User(buyerName, buyerPhoneNumber, buyerAddress);
     }
 
