acaef361868bf386c89fa8c77f1d7ae20ed6cb21
eric <eric.wang@cargosmart.com>
2021-06-22 15:55:43 +0800

    refactor: move getCity and getProvince

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -1,13 +1,21 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class Address {
-    private final String fromAddress;
+    private final String address;
 
-    public Address(String fromAddress) {
-        this.fromAddress = fromAddress;
+    public Address(String address) {
+        this.address = address;
     }
 
     public String getFromAddress() {
-        return fromAddress;
+        return address;
+    }
+
+    private String getCity() {
+        return address.substring(address.indexOf("省") + 1, address.indexOf("市"));
+    }
+
+    private String getProvince() {
+        return address.substring(0, address.indexOf("省"));
     }
 }
