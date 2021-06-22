10a919162e11b2f6b8f770fe380b6711b8e06e97
eric <eric.wang@cargosmart.com>
2021-06-22 16:10:05 +0800

    refactor: remove not use address field

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -1,12 +1,10 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class Address {
-    private final String address;
     private final String province;
     private final String city;
 
     public Address(String address) {
-        this.address = address;
         this.province = address.substring(0, address.indexOf("省"));
         this.city = address.substring(address.indexOf("省") + 1, address.indexOf("市"));
     }
