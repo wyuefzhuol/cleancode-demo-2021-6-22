af9a86e7147e79f58daff590d5482c8a5f0aa39a
eric <eric.wang@cargosmart.com>
2021-06-22 14:23:57 +0800

    refactor: introduce User create Order-buyer field

--- src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -1,4 +1,25 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class User {
+    private final String name;
+    private final String phoneNumber;
+    private final String address;
+
+    public User(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
